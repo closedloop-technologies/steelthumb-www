b30872c20ac95eb3828d4a0482f9b509f753fb6b
Sean Kruzel <sean.kruzel@gmail.com>
2024-12-14 06:27:26 -0500

    build ssr

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,16 @@
 
 import type { Config } from "@react-router/dev/config";
+
+// export default {
+//     ssr: false,
+// } satisfies Config;
+
 export default {
-    ssr: false,
+    // all static route paths
+    // (no dynamic segments like "/post/:slug")
+    prerender: true,
+
+    // any url
+    //   prerender: ["/", "/blog", "/blog/popular-post"],
+
 } satisfies Config;
